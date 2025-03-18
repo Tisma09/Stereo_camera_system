fba0a6e360e60cf60ec5d097fe9dac0a5853a383
Tismo <deywostismo@gmail.com>
2025-03-18 15:11:30 +0100

    Create project

---
+++ camera.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+
+class Camera():
+
+    def __init__(self):
+        pass
+    
+
+    #####################################################
+    #############    Camera Calibration    ##############
+    #####################################################
+
+    def calibration(self):
+        pass
+
---
+++ main.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as np
+
+def main():
+    print("Temp")
+
+
+if __name__ == "__main__":
+    main()
+
+
